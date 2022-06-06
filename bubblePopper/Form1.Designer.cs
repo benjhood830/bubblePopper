001075a9a45702ed59181d640fe39ff3b686899e
benjhood830 <benjhood830@SDS-L-FGHDF63>
2022-06-06 13:34:22 -0400

    Add project files.

---
+++ bubblePopper/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace bubblePopper
+{
+    partial class bubblePopper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(bubblePopper));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subtitleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Arcade", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.Lime;
+            this.titleLabel.Location = new System.Drawing.Point(22, 32);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(542, 32);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "BUBBLE POPPER";
+            // 
+            // subtitleLabel
+            // 
+            this.subtitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subtitleLabel.Font = new System.Drawing.Font("Arcade", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtitleLabel.ForeColor = System.Drawing.Color.White;
+            this.subtitleLabel.Location = new System.Drawing.Point(7, 64);
+            this.subtitleLabel.Name = "subtitleLabel";
+            this.subtitleLabel.Size = new System.Drawing.Size(497, 32);
+            this.subtitleLabel.TabIndex = 1;
+            this.subtitleLabel.Text = "Press Space to Begin or ESC to Exit";
+            // 
+            // bubblePopper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::bubblePopper.Properties.Resources.Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(400, 600);
+            this.Controls.Add(this.subtitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "bubblePopper";
+            this.Text = "Bubble Popper";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.bubblePopper_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.bubblePopper_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.bubblePopper_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subtitleLabel;
+    }
+}
+
---
+++ bubblePopper/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bubblePopper
+{
+    public partial class bubblePopper : Form
+    {
+        Rectangle player = new Rectangle(280, 540, 40, 10);
+        int playerSpeed = 10;
+
+        List<Rectangle> greenBubbles = new List<Rectangle>();
+        List<Rectangle> redBubbles = new List<Rectangle>();
+        List<Rectangle> blueBubbles = new List<Rectangle>();
+        List<Rectangle> yellowBubbles = new List<Rectangle>();
+        List<Rectangle> playerShots = new List<Rectangle>();
+
+        List<int> bubbleSpeeds = new List<int>();
+        //List<string> bubbleColours = new List<string>();
+        int bubbleSize = 20;
+        int playershotSize = 14;
+
+        int score = 0;
+
+        bool ADown = false;
+        bool DDown = false;
+        bool spaceDown = false;
+        bool escDown = false;
+
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush greenBrush = new SolidBrush(Color.Green);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+        SolidBrush blueBrush = new SolidBrush(Color.Blue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush purpleBrush = new SolidBrush(Color.Purple);
+
+
+        Pen blackPen = new Pen(Color.Black, 3);
+
+        string gameState = "waiting";
+
+
+        Random randGen = new Random();
+        int randValue = 0;
+
+        public bubblePopper()
+        {
+            InitializeComponent();
+        }
+
+        private void GameInitialize()
+        {
+            titleLabel.Text = "";
+            subtitleLabel.Text = "";
+
+
+            player.X = this.ClientSize.Width / 2 - player.Width / 2;
+            player.Y = this.ClientSize.Height - player.Height;
+                ;
+            player.Width = 40;
+            player.Height = 10;
+
+            gameTimer.Enabled = true;
+
+            gameState = "running";
+
+
+        }
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+
+            int randValue = randGen.Next(0, 401);
+
+            if (randValue < 100)
+            {
+                int x = randGen.Next(0, 80);
+                greenBubbles.Add(new Rectangle(x, 0, bubbleSize, bubbleSize));
+
+                bubbleSpeeds.Add(randGen.Next(1, 1));
+            }
+
+            int playershotX = player.X;
+            int playershotY = player.Y;
+
+            if(spaceDown == true && gameState == "running")
+            {
+                playerShots.Add(new Rectangle(playershotX, playershotY, playershotSize, playershotSize));
+               
+            }
+            
+            
+            
+            
+            
+            //for (int i = 0; i < greenBubbles.Count(); i++)
+            //{
+            //    while(greenBubbles.IntersectWith(greenBubbles))
+
+            //}
+
+
+                gameTimer.Enabled = true;
+
+            if (gameState == "waiting")
+            {
+                // player.Size = new Size(0, 0);
+            }
+            //move player 
+            if (ADown == true && player.X > 0 + 4)
+            {
+                player.X -= playerSpeed;
+            }
+
+            if (DDown == true && player.X < this.ClientSize.Width - player.Width - 4)
+            {
+                player.X += playerSpeed;
+            }
+
+            // move balls 
+            for (int i = 0; i < greenBubbles.Count(); i++)
+            {
+                //find the new postion of y based on speed 
+                int y = greenBubbles[i].Y + bubbleSpeeds[i];
+
+                //replace the rectangle in the list with updated one using new y 
+                greenBubbles[i] = new Rectangle(greenBubbles[i].X, y, bubbleSize, bubbleSize);
+            }
+            
+            Refresh();
+        }
+
+        private void bubblePopper_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    ADown = false;
+                    break;
+                case Keys.D:
+                    DDown = false;
+                    break;
+                case Keys.Space:
+                    spaceDown = false;
+                    break;
+                case Keys.Escape:
+                    escDown = false;
+                    break;
+            }
+        }
+
+        private void bubblePopper_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    ADown = true;
+                    break;
+                case Keys.D:
+                    DDown = true;
+                    break;
+                case Keys.Space:
+
+                    if (gameState == "waiting" || gameState == "over")
+
+                    {
+                        //gameTimer.Enabled = true;
+                        GameInitialize();
+                    }
+                    break;
+
+                case Keys.Escape:
+
+                    if (gameState == "waiting" || gameState == "over")
+
+                    {
+                        Application.Exit();
+                    }
+                    break;
+            }
+        }
+
+        private void bubblePopper_Paint(object sender, PaintEventArgs e)
+        {
+
+            if (gameState == "running")
+            {
+                e.Graphics.FillRectangle(whiteBrush, player);
+                e.Graphics.DrawRectangle(blackPen, player);
+                for (int i = 0; i < playerShots.Count; i++)
+                {
+                    e.Graphics.FillRectangle(purpleBrush, playerShots[i]);
+                    e.Graphics.DrawEllipse(blackPen, playerShots[i]);
+                }
+                for(int i = 0; i < greenBubbles.Count; i++)
+                {
+                    e.Graphics.DrawEllipse(blackPen, greenBubbles[i]);
+                    e.Graphics.FillEllipse(greenBrush, greenBubbles[i]);
+                }
+                
+                }
+
+            }
+
+
+        }
+
+    }
+
+
+
